7527a97decd59bc39fbcd93675bef4f0b381d6d8
christiansaccani <christian.saccani99@gmail.com>
2024-02-13 09:57:58 +0100

    snack 1 optimized

--- Snack1/script.js
+++ Snack1/script.js
@@ -2,10 +2,13 @@ const startBtn = document.querySelector("#start-btn");
 let solutionEl = document.querySelector("#solution")
 
 function reverse (userChoice) {
-    let split = userChoice.split(""); //questo comando mi restituisce un array
-    let reverse = split.reverse();
-    let join = reverse.join(""); //questo comando unisce l'array in una stringa
-    return join;
+    // let split = userChoice.split(""); //questo comando mi restituisce un array
+    // let reverse = split.reverse();
+    // let join = reverse.join(""); //questo comando unisce l'array in una stringa
+    // return join;
+
+    const result = userChoice.split("").reverse().join("");
+    return result;
 }
 
 startBtn.addEventListener("click", function(event) {
